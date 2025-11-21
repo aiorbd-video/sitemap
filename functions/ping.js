4b4b04e1cd87ef96fc203928df6dc65b75c9dd65
aiorbd-video <ratulhasan5a@gmail.com>
2025-11-21 21:45:07 +0600

    Update ping.js

--- functions/ping.js
+++ functions/ping.js
@@ -1,1 +1,8 @@
+export async function onRequest() {
+  const sitemap = "https://sitemap-cvs.pages.dev/sitemap";
 
+  await fetch(`https://www.google.com/ping?sitemap=${sitemap}`);
+  await fetch(`https://www.bing.com/ping?sitemap=${sitemap}`);
+
+  return new Response("Pings sent");
+}
